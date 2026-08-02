263902c75d75999e5a49271f1313684585561900
晴曛 <3316361825@qq.com>
2026-08-02 17:28:47 +0800

    图片编辑功能开发

--- pythonFlask/server.py
+++ pythonFlask/server.py
@@ -1,6 +1,12 @@
 import base64
+import json
 import os
+import tempfile
+import uuid
+
 from PIL import Image
+from dashscope import MultiModalConversation
+from deepagents import create_deep_agent
 
 from flask import Flask, request, jsonify
 from io import BytesIO
@@ -8,31 +14,38 @@ from io import BytesIO
 from flask.cli import load_dotenv
 from langchain_community.chat_models.tongyi import ChatTongyi
 from langchain_core.messages.human import HumanMessage
+from langchain_core.messages.tool import tool_call
 from langchain_core.prompts.chat import ChatPromptTemplate
-
+from langchain_core.tools import tool
 
 # 创建Flask应用实例
 app = Flask(__name__)
 
-# 获取配置信息
+# 获取配置信息（从项目根目录下的 .env 文件中读取键值对，并将其加载到当前进程的环境变量（os.environ）中）
 load_dotenv()
-
 API_KEY = os.getenv("DASHSCOPE_API_KEY")
 
 # 将图片bytes转换成base_uri
 def process_image(image_data : bytes) -> str:
+    # 将字节码转成 可操作的图像字节码对象
     img = Image.open(BytesIO(image_data))
+
+    # 获取文件后缀
     image_format = (img.format).lower()
+
+    # 获得base64字节码
     image_base64 = base64.b64encode(image_data).decode("utf-8")
+
+    # 拼接uri
     data_uri = f"data:{image_format};base64,{image_base64}"
     return data_uri
 
 # 调用大模型生成图片文字描述信息
 def describe_image(image_data : bytes) -> str:
     try:
-        # 1. 先把图片转化成base64
+        # 1. 先把图片转化成base64格式的字节码
         data_uri = process_image(image_data)
-        # 2. 构建LangChain的请求
+        # 2. 构建LangChain的请求（这里借助的是大模型的多模态能力，所以不使用模板提示词构建，模板提示词适用于大模型聊天能力）
         human_content = [
             {"image": data_uri},
             {
@@ -96,7 +109,10 @@ def validate_image(image_desc : str) -> bool:
 @app.route("/api/validate-image", methods = ["POST"])
 def validate_image_api():
     try:
+        # 从请求中获取可操作的file对象
         file = request.files["file"]
+
+        # 从文件中读出字节码
         image_data = file.read()
         desc = describe_image(image_data) # 调用大模型生成图片文字描述信息
         allow = validate_image(desc)     # 二次调用大模型，处理文字描述信息判断当前图片是否符合要求
@@ -104,6 +120,171 @@ def validate_image_api():
     except Exception as e:
         print(f"执行审核图片操作捕获异常:{e}")
         return jsonify({"code":500, "allow": False}), 500
+
+
+
+# 全局的agent智能体
+deep_agent=None
+
+# 配置大模型实例参数
+llm=ChatTongyi(
+    model_name="qwen-plus",
+    temperature=0.1,
+    dashscope_api_key=API_KEY
+)
+
+
+# 创建工具
+@tool
+def edit_image_tool(image_path:str,instruction:str)->str:
+    """编辑单张图片"
+    输入本地图片地址路径与编辑指令  调用Dashscope的图片编辑模型生成新的URL
+    返回JSON字符串：{"success":true,"url":"..."}或{"success":false,"error":"..."}
+    """
+    try:
+        # 读取文件内容
+        with open(image_path, "rb") as f:
+            image_data = f.read()
+
+        # 转换字节文件
+        image_data_uri = process_image(image_data)
+
+        # 提示词构建
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "image": image_data_uri
+                    },
+                    {
+                        "text": instruction
+                    }
+                ]
+            }
+        ]
+        # 构建请求大模型参数
+        params = {
+            "model": "qwen-image-edit-plus",
+            "messages": messages
+        }
+
+        # 调用Dashscope编辑模型
+        response = MultiModalConversation.call(**params)
+        # 获取url
+        url = response['output']['choices'][0]['message']['content'][0]["image"]
+        # 将python文件从dict转为字符串，ensure_ascii=False：允许非ASCII字符（如中文）正常显示
+        return json.dumps({"success": True, "url": url}, ensure_ascii=False)
+    except Exception as e:
+        print(f"调用编辑模型报错：{e}")
+        return json.dumps({"success": False, "error": f"{e}"}, ensure_ascii=False)
+
+
+# 声明工具
+skills_tools=[edit_image_tool]
+
+# 创建agent
+try:
+    deep_agent = create_deep_agent(
+        model=llm,
+        tools=skills_tools,
+        skills=["/skills/"],
+        system_prompt=(
+            "你是一个智能图片处理助手，你可以调用一个工具: \n"
+            "- edit_image_tool：编辑单张图片 \n"
+            "最终的输出格式JSON：{\"success\":true,\"url\":\"字符串类型\"} 或者{\"success\":false,\"error\":\"字符串类型\"}"
+        )
+    )
+    print("Agent 已启用")
+except Exception as e:
+    print("Agent未启用")
+    deep_agent=None
+
+
+# agent执行
+def invoke_agent(param:str)->dict:
+   try:
+       # 执行
+       state = deep_agent.invoke(
+           {
+               "messages": [
+                   {
+                       "role": "user",
+                       "content": param
+                   }
+               ]
+           }
+       )
+
+       msg = (state or {}).get("messages") or []
+
+       last = msg[-1] if msg else None
+       content = last.content
+       # 解析json，将JSON字符串解析为Python字典
+       obj = json.loads(content)
+       return obj
+   except Exception as e:
+       print(f"agent函数执行异常:{e}")
+       return {"success":False,"error":f"执行agent函数失败{e}"}
+
+# 定义编辑/合并图片的的接口路由
+def skill_image()->str:
+   try:
+       # 校验大模型实例是否为空
+       if deep_agent is None:
+           return ""
+       # 获取参数（编辑指令）
+       instruction = request.form.get("instruction")
+       # 判断编辑指令是否为空
+       if not instruction:
+           return ""
+       # 临时保存地址
+       tmp_dir = tempfile.gettempdir()
+       # 临时路径
+       tem_paths = []
+       # 提示词
+       prompt_lines = [
+           "你必须调用一个工具，并且只输出 JSON格式",
+           f"instruction:{instruction}"
+       ]
+       # 获取客户端发送给服务器的文件
+       data = request.files["file"].read();
+       # 本地保存临时文件
+       p = os.path.join(tmp_dir, f"aiwear_{uuid.uuid4().hex}.bin")
+       # 将data内容写入文件p中
+       with open(p,"wb") as f:
+           f.write(data)
+
+       tem_paths.append(p)
+       prompt_lines.append(f"image_path:{p}")
+       # 调用大模型进行处理
+       out = invoke_agent("\n".join(prompt_lines))
+       return out["url"]
+   except Exception as e:
+       print(f"执行skill异常{e}")
+       return ""
+
+@app.route("api/skill/image",methods=["POST"])
+def skill_image_api():
+    try:
+        out=skill_image()
+        return jsonify(
+            {
+                "success": True,
+                "out":out,
+            }
+        ),200
+    except Exception as e:
+        print(f"调用skill处理失败{e}")
+        return jsonify(
+            {
+                "success":False,
+                "error":str(e)
+            }
+        ),500
+
+
+
 
 
 
